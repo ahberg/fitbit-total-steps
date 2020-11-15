8198e384d26fe1d982ec50891411fae2530912c6
Anton <anton@happyorder.io>
2020-11-15 09:48:12 +0100

    0.1 works

--- index.js
+++ index.js
@@ -1,35 +1,68 @@
-const fs = require('fs');
-let dayLog = []
+const fs = require("fs");
+let stepsFolder = "steps/";
+let dayLog = [];
+let currentDay = 0;
+let objects = [];
 
 function parseDate(dateTime) {
-  let d = dateTime.substring(0, 8)
-  d = d.split('/')
-  console.log(d);
-  return '20' + d[2]  + '-' + d[0] + '-' + d[1] 
+  let d = dateTime.substring(0, 8);
+  d = d.split("/");
+  return "20" + d[2] + "-" + d[0] + "-" + d[1];
 }
 
-fs.readFile('steps/steps-2020-06-28.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    let stepsTotal  = 0;
-    let object = JSON.parse(data);
-    let currentDay = 0;
-    object.forEach((m, index, arr) => {
-        let date = new Date(m.dateTime);
-        let dateISO =  parseDate(m.dateTime);
-        if(currentDay === 0) {
-            currentDay = dateISO;
+let files = fs.readdirSync(stepsFolder);
+files.forEach((jsonFile) => {
+  let content = fs.readFileSync(stepsFolder + jsonFile, "utf8");
+  objects.push(...JSON.parse(content));
+});
+
+stepsTotal = 0;
+objects.forEach((m, index, arr) => {
+  let dateISO = parseDate(m.dateTime);
+  if (currentDay === 0) {
+    currentDay = dateISO;
+  } else {
+    if (currentDay !== dateISO || index === arr.length - 1) {
+      let day = {
+        date: currentDay,
+        steps: stepsTotal,
+      };
+      dayLog.push(day);
+      currentDay = dateISO;
+      stepsTotal = 0;
+    }
+  }
+  stepsTotal = stepsTotal + parseInt(m.value);
+});
+console.log(objects.length)
+//console.log(JSON.stringify(dayLog));
+
+return;
+fs.readdir(stepsFolder)
+  .forEach((jsonFile) => {
+    fs.readFile(stepsFolder + jsonFile, "utf8", (err, data) => {
+      if (err) throw err;
+      let stepsTotal = 0;
+      let object = JSON.parse(data);
+      object.forEach((m, index, arr) => {
+        let dateISO = parseDate(m.dateTime);
+        if (currentDay === 0) {
+          currentDay = dateISO;
         } else {
-            if(currentDay !== dateISO || index === arr.length - 1){
-                let day = {
-                    'date': currentDay,
-                    'steps': stepsTotal,
-                }
-                dayLog.push(day);
-                currentDay = dateISO;
-                stepsTotal = 0
-            }
+          if (currentDay !== dateISO || index === arr.length - 1) {
+            let day = {
+              date: currentDay,
+              steps: stepsTotal,
+            };
+            dayLog.push(day);
+            currentDay = dateISO;
+            stepsTotal = 0;
+          }
         }
         stepsTotal = stepsTotal + parseInt(m.value);
-    })
-    console.log(dayLog);
+      });
+      console.log(dayLog);
+    });
+    return 0;
   })
+  .then(() => console.log(dayLog));
