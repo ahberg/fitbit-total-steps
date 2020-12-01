7615b377a99bfaad48098690baf4e42f367020bf
Anton <anton@happyorder.io>
2020-12-01 18:46:43 +0100

    Added standard eslint

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-const fs = require("fs");
+const fs = require('fs')
 
-const stepsFolder = "steps/";
-const dayLog = [];
-let currentDay = 0;
-const objects = [];
+const stepsFolder = 'steps/'
+const dayLog = []
+let currentDay = 0
+const objects = []
 
-function parseDate(dateTime) {
-  let d = dateTime.substring(0, 8);
-  d = d.split("/");
-  return `20${d[2]}-${d[0]}-${d[1]}`;
+function parseDate (dateTime) {
+  let d = dateTime.substring(0, 8)
+  d = d.split('/')
+  return `20${d[2]}-${d[0]}-${d[1]}`
 }
 
-const files = fs.readdirSync(stepsFolder);
+const files = fs.readdirSync(stepsFolder)
 files.forEach((jsonFile) => {
-  const content = fs.readFileSync(stepsFolder + jsonFile, "utf8");
-  objects.push(...JSON.parse(content));
-});
+  const content = fs.readFileSync(stepsFolder + jsonFile, 'utf8')
+  objects.push(...JSON.parse(content))
+})
 
-let stepsTotal = 0;
+let stepsTotal = 0
 objects.forEach((m, index, arr) => {
-  const dateISO = parseDate(m.dateTime);
+  const dateISO = parseDate(m.dateTime)
   if (currentDay === 0) {
-    currentDay = dateISO;
+    currentDay = dateISO
   } else if (currentDay !== dateISO || index === arr.length - 1) {
     const day = {
       date: currentDay,
-      steps: stepsTotal,
-    };
-    dayLog.push(day);
-    currentDay = dateISO;
-    stepsTotal = 0;
+      steps: stepsTotal
+    }
+    dayLog.push(day)
+    currentDay = dateISO
+    stepsTotal = 0
   }
-  stepsTotal += parseInt(m.value, 10);
-});
+  stepsTotal += parseInt(m.value, 10)
+})
 
-dayLog.sort((a, b) => b.steps - a.steps);
+dayLog.sort((a, b) => b.steps - a.steps)
 
 const csvArray = dayLog.map((obj) =>
   Object.keys(obj).reduce((arr, current) => {
-    arr.push(obj[current]);
-    return arr;
+    arr.push(obj[current])
+    return arr
   }, [])
-);
-csvArray.unshift(["Dates", "Steps"]);
+)
+csvArray.unshift(['Dates', 'Steps'])
 
-
-const CSV = csvArray.join("\n");
-fs.writeFileSync("allDaysSteps.json", JSON.stringify(dayLog));
-fs.writeFileSync("allDaysSteps.csv", CSV);
+const CSV = csvArray.join('\n')
+fs.writeFileSync('allDaysSteps.json', JSON.stringify(dayLog))
+fs.writeFileSync('allDaysSteps.csv', CSV)
