cfffd938295a83b150a800a10968d92602652ee3
Anton <anton@happyorder.io>
2020-12-01 17:43:17 +0100

    Create a json and csv file

--- index.js
+++ index.js
@@ -1,68 +1,50 @@
 const fs = require("fs");
-let stepsFolder = "steps/";
-let dayLog = [];
+
+const stepsFolder = "steps/";
+const dayLog = [];
 let currentDay = 0;
-let objects = [];
+const objects = [];
 
 function parseDate(dateTime) {
   let d = dateTime.substring(0, 8);
   d = d.split("/");
-  return "20" + d[2] + "-" + d[0] + "-" + d[1];
+  return `20${d[2]}-${d[0]}-${d[1]}`;
 }
 
-let files = fs.readdirSync(stepsFolder);
+const files = fs.readdirSync(stepsFolder);
 files.forEach((jsonFile) => {
-  let content = fs.readFileSync(stepsFolder + jsonFile, "utf8");
+  const content = fs.readFileSync(stepsFolder + jsonFile, "utf8");
   objects.push(...JSON.parse(content));
 });
 
-stepsTotal = 0;
+let stepsTotal = 0;
 objects.forEach((m, index, arr) => {
-  let dateISO = parseDate(m.dateTime);
+  const dateISO = parseDate(m.dateTime);
   if (currentDay === 0) {
     currentDay = dateISO;
-  } else {
-    if (currentDay !== dateISO || index === arr.length - 1) {
-      let day = {
-        date: currentDay,
-        steps: stepsTotal,
-      };
-      dayLog.push(day);
-      currentDay = dateISO;
-      stepsTotal = 0;
-    }
+  } else if (currentDay !== dateISO || index === arr.length - 1) {
+    const day = {
+      date: currentDay,
+      steps: stepsTotal,
+    };
+    dayLog.push(day);
+    currentDay = dateISO;
+    stepsTotal = 0;
   }
-  stepsTotal = stepsTotal + parseInt(m.value);
+  stepsTotal += parseInt(m.value, 10);
 });
-console.log(objects.length)
-//console.log(JSON.stringify(dayLog));
 
-return;
-fs.readdir(stepsFolder)
-  .forEach((jsonFile) => {
-    fs.readFile(stepsFolder + jsonFile, "utf8", (err, data) => {
-      if (err) throw err;
-      let stepsTotal = 0;
-      let object = JSON.parse(data);
-      object.forEach((m, index, arr) => {
-        let dateISO = parseDate(m.dateTime);
-        if (currentDay === 0) {
-          currentDay = dateISO;
-        } else {
-          if (currentDay !== dateISO || index === arr.length - 1) {
-            let day = {
-              date: currentDay,
-              steps: stepsTotal,
-            };
-            dayLog.push(day);
-            currentDay = dateISO;
-            stepsTotal = 0;
-          }
-        }
-        stepsTotal = stepsTotal + parseInt(m.value);
-      });
-      console.log(dayLog);
-    });
-    return 0;
-  })
-  .then(() => console.log(dayLog));
+dayLog.sort((a, b) => b.steps - a.steps);
+
+const csvArray = dayLog.map((obj) =>
+  Object.keys(obj).reduce((arr, current) => {
+    arr.push(obj[current]);
+    return arr;
+  }, [])
+);
+csvArray.unshift(["Dates", "Steps"]);
+
+
+const CSV = csvArray.join("\n");
+fs.writeFileSync("allDaysSteps.json", JSON.stringify(dayLog));
+fs.writeFileSync("allDaysSteps.csv", CSV);
